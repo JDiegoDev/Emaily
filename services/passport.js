27e25433906317ab64a12b9fa216e1c99b401635
Juan Diego Gimenez Pena <juandiego.gimenez@globant.com>
2021-05-20 13:25:10 -0500

    adding dev enviroment to git ignore

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: `${keys.googleRedirectURI}/auth/google/callback`,
+      callbackURL: "/auth/google/callback",
       proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
