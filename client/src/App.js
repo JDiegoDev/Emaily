869304dbc3237e44cccf7551702a0f6c6f267fc1
Juan Diego Gimenez Pena <juandiego.gimenez@globant.com>
2021-06-08 13:26:21 -0500

    refactor promises with async syntax

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/auth/google"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sign In with google
         </a>
       </header>
     </div>
