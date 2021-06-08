f6aa23f7a861f39897adbf76bcd69d2a82576e60
Juan Diego Gimenez Pena <juandiego.gimenez@globant.com>
2021-06-08 17:18:25 -0500

    designing Header file

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header !!!</div>;
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a className="left brand-logo">Emaily</a>
+        <ul className="right">
+          <li>
+            <a>Login with Google</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
