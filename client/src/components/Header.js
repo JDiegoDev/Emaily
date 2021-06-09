84ce9059813e77ef2a0dde67e02e743e7015990b
Juan Diego Gimenez Pena <juandiego.gimenez@globant.com>
2021-06-09 14:29:18 -0500

    handling login and logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const user = useSelector((state) => state.auth);
+
+  const renderHeaderOption = () => {
+    switch (user) {
+      case null:
+        return;
+
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">login with google</a>
+          </li>
+        );
+
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  };
+
   return (
     <nav>
       <div className="nav-wrapper">
         <a className="left brand-logo">Emaily</a>
-        <ul className="right">
-          <li>
-            <a>Login with Google</a>
-          </li>
-        </ul>
+        <ul className="right">{renderHeaderOption()}</ul>
       </div>
     </nav>
   );
