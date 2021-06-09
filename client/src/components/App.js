c53ce31b11fba080378b297673f47a7f97f67d38
Juan Diego Gimenez Pena <juandiego.gimenez@globant.com>
2021-06-09 13:34:40 -0500

    refactor action creator

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
+import * as actions from "../actions";
+import { useDispatch } from "react-redux";
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(actions.fetchUser());
+  });
+
   return (
     <div className="container">
       <BrowserRouter>
